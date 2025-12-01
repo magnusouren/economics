26f3396a77642ec0d433ec27f3e981fea24bb1d8
magnusouren <magnut0203@gmail.com>
2025-12-01 17:18:39 +0100

    auto generated student loan

--- views/studentLoan.tsx
+++ views/studentLoan.tsx
@@ -1,9 +1,40 @@
-import React from 'react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Trash } from 'lucide-react';
 import useStore, { StoreState } from '@/lib/store';
 import type { Loan } from '@/types';
+import {
+    NativeSelect,
+    NativeSelectOption,
+} from '@/components/ui/native-select';
+import { Label } from '@/components/ui/label';
+
+const loanAmounts: Record<string, number> = {
+    '2025-2026': 166859,
+    '2024-2025': 151960,
+    '2023-2024': 137907,
+    '2022-2023': 128889,
+    '2021-2022': 126357,
+    '2020-2021': 123519,
+    '2019-2020': 121220,
+    '2018-2019': 116369,
+    '2017-2018': 111657,
+    '2016-2017': 106549,
+    '2015-2016': 100902,
+    '2014-2015': 97850,
+    '2013-2014': 94400,
+    '2012-2013': 92500,
+    '2011-2012': 90800,
+    '2010-2011': 89000,
+    '2009-2010': 87600,
+    '2008-2009': 85000,
+    '2007-2008': 82900,
+    '2006-2007': 81400,
+    '2005-2006': 80000,
+    '2004-2005': 80000,
+    '2003-2004': 80000,
+    '2002-2003': 80000,
+};
 
 export default function StudentLoan() {
     const loans = useStore((s: StoreState) => s.data.loans);
@@ -28,6 +59,46 @@ export default function StudentLoan() {
         0
     );
 
+    const parseStartYear = (range: string) => Number(range.split('-')[0]);
+
+    const sortedRanges = Object.entries(loanAmounts).sort((a, b) => {
+        const ay = parseStartYear(a[0]);
+        const by = parseStartYear(b[0]);
+        return ay - by; // eldste -> nyeste
+    });
+
+    function handleAutoFill() {
+        const selects = document.getElementsByTagName('select');
+        const fromLabel =
+            selects[selects.length - 2].selectedOptions[0].textContent!;
+        const toLabel =
+            selects[selects.length - 1].selectedOptions[0].textContent!;
+
+        const fromYear = parseStartYear(fromLabel);
+        const toYear = parseStartYear(toLabel);
+
+        let total = 0;
+
+        for (const [range, amount] of sortedRanges) {
+            const year = parseStartYear(range);
+            if (year >= fromYear && year <= toYear) {
+                total += amount;
+            }
+        }
+
+        const loanAmount = total * 0.6;
+
+        handleAdd();
+        handleUpdate(loans.length, {
+            description: `Studielån ${loans.length + 1}`,
+            loanAmount: loanAmount,
+            interestRate: 4.698,
+            termYears: 20,
+            termsPerYear: 12,
+            monthlyFee: 0,
+        });
+    }
+
     return (
         <section className='w-full my-8'>
             <div className='flex items-center justify-between mb-2'>
@@ -36,11 +107,52 @@ export default function StudentLoan() {
                     + Legg til lån
                 </Button>
             </div>
+
             <p className='mt-2 mb-4 text-muted-foreground'>
                 Legg inn informasjon om ditt studielån her. Du kan legge til
                 flere lån dersom du har flere. Du kan også legge til andre typer
                 lån her dersom du har det.
             </p>
+
+            <div className='mt-2 mb-4 text-muted-foreground flex gap-2 justify-start align-center'>
+                <p className='mt-1.5'>
+                    Forhåndsutfyll lånebeløp basert på når du studerte:
+                </p>
+
+                <div className='flex justify-start gap-2'>
+                    {/* FROM */}
+                    <Label htmlFor='from-year' className='mt-2'>
+                        Fra
+                    </Label>
+                    <NativeSelect name='from-year'>
+                        {Object.entries(loanAmounts).map(([year, amount]) => (
+                            <NativeSelectOption key={year} value={amount}>
+                                {year}
+                            </NativeSelectOption>
+                        ))}
+                    </NativeSelect>
+
+                    {/* TO */}
+                    <Label htmlFor='to-year' className='mt-2'>
+                        Til
+                    </Label>
+                    <NativeSelect name='to-year'>
+                        {Object.entries(loanAmounts).map(([year, amount]) => (
+                            <NativeSelectOption key={year} value={amount}>
+                                {year}
+                            </NativeSelectOption>
+                        ))}
+                    </NativeSelect>
+
+                    <Button
+                        variant='outline'
+                        size='sm'
+                        onClick={handleAutoFill}
+                    >
+                        Sett lånebeløp
+                    </Button>
+                </div>
+            </div>
 
             {loans.length !== 0 && (
                 <div className='overflow-auto rounded-md border'>
@@ -60,8 +172,15 @@ export default function StudentLoan() {
                             {loans.map((loan, idx) => (
                                 <tr key={idx} className='align-top'>
                                     <td className='p-2'>
-                                        {loan.description ||
-                                            `Studielån ${idx + 1}`}
+                                        <Input
+                                            type='text'
+                                            value={loan.description}
+                                            onChange={(e) =>
+                                                handleUpdate(idx, {
+                                                    description: e.target.value,
+                                                })
+                                            }
+                                        />
                                     </td>
                                     <td className='p-2'>
                                         <Input
@@ -134,7 +253,7 @@ export default function StudentLoan() {
                                             variant='outline'
                                             size='icon-sm'
                                             onClick={() => handleDelete(idx)}
-                                            className=' text-destructive border-destructive hover:bg-destructive/10 hover:border-destructive hover:text-destructive'
+                                            className='text-destructive border-destructive hover:bg-destructive/10 hover:border-destructive hover:text-destructive'
                                         >
                                             <Trash />
                                         </Button>
